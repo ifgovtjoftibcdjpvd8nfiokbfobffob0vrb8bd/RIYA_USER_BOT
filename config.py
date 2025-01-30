b7995e91b5cc72e7ea569b0da0fabb1043a51387
ifgovtjoftibcdjpvd8nfiokbfobffob0vrb8bd <sonkarp463@gmail.com>
2025-01-31 00:43:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,9 +10,9 @@ API_ID = int(getenv("API_ID", "27079591")) #optional
 API_HASH = getenv("API_HASH", "c81ae4c3dc026ea4bf49842a8ce4a5f9") #optional
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "7668520999").split()))
-OWNER_ID = int(getenv("OWNER_ID"))
-MONGO_URL = getenv("MONGO_URL")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
+OWNER_ID = int(getenv("OWNER_ID", 8009369344))
+MONGO_URL = getenv("MONGO_URL","mongodb+srv://theriyamusic94:f67KlgTyzr3TTutn@cluster0.lym5x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+BOT_TOKEN = getenv("BOT_TOKEN", "7907566055:AAFXzaaqXYBraXLs50OhFyMqN8g9BK8zW4c")
 ALIVE_PIC = getenv("ALIVE_PIC", "https://files.catbox.moe/2731lh.jpg")
 ALIVE_TEXT = getenv("ALIVE_TEXT")
 PM_LOGGER = getenv("PM_LOGGER")
@@ -21,7 +21,7 @@ GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
 REPO_URL = getenv("REPO_URL", "https://github.com/ifgovtjoftibcdjpvd8nfiokbfobffob0vrb8bd/RIYA_USER_BOT")
 BRANCH = getenv("BRANCH", "main") #don't change
  
-STRING_SESSION1 = getenv("STRING_SESSION1", "")
+STRING_SESSION1 = getenv("STRING_SESSION1", "ATUAAv4AAQEAATSwc3GI-tW6F0ztIQSuc6PMbrtlpuw1DDoJsT4zAmCjzgpz_jH14uJK_x08GG4nqsgFIlCkKORp7II4iulEWWsJVWiicBvf4VLmdN0Ytv5Xr9Rz6uXTwz9dndGkGfIs3hbLEFjq04d_2Uw2_jrzymfEK-KrCGr5fg4g_3czLmtPXTDIlr6EcPKhdNcsEk4Jlflph3x-6l9XrvQBifkRGk4UEpSGPxmKd-GuugWD_Mbu2doXeCEPacAeCh4YgUjFl8g-bC_MPGoyKVuUNcwSfGjhSW-i7bpJWuMrWqtJRYyVQ-617fnKqYCIuIke-CLRqcudJkIrK-cGsOif9EDlzh8F8JEBAAExo308AQAD")
 STRING_SESSION2 = getenv("STRING_SESSION2", "")
 STRING_SESSION3 = getenv("STRING_SESSION3", "")
 STRING_SESSION4 = getenv("STRING_SESSION4", "")
